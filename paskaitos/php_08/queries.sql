6b1c4fdd702a8cebd71778abb494cb14055c49cd
jarmolovskis <kazimiras.jarmolovskis@gmail.com>
2024-04-11 19:11:37 +0300

    Paskaitos kodas

--- paskaitos/php_08/queries.sql
+++ paskaitos/php_08/queries.sql
@@ -24,3 +24,106 @@ SELECT * FROM `knygos` WHERE `kaina` > 19 OR `kaina` < 10 OR `puslapiu_skaicius`
 
 -- Kelios salygos (Loginis operatorius OR (arba) ir AND (ir) tada lengviau rašyti su kliaustais)
 SELECT * FROM `knygos` WHERE (`kaina` > 15 AND `kaina` < 20) OR (`puslapiu_skaicius` > 300 AND `puslapiu_skaicius` < 600)
+
+-- NOT yra salygos priešingybė
+SELECT * FROM `knygos` WHERE `kaina` != 7.80
+SELECT * FROM `knygos` WHERE NOT `kaina` = 7.80
+SELECT * FROM `knygos` WHERE `kaina` IS NULL
+SELECT * FROM `knygos` WHERE `kaina` IS NOT NULL
+
+-- Uzklausa su salygos sarasu
+SELECT * FROM `knygos` WHERE `kaina` IN (7.80, 12.50, 50.99)
+-- Tokia pati uzklausa tik kitaip
+SELECT * FROM `knygos` WHERE `kaina` = 7.80 OR `kaina` = 12.50 OR `kaina` = 50.99
+-- SUBQUERY, uzklausa uzklausoje.
+SELECT *
+FROM `knygos`
+WHERE `id` IN (
+    SELECT `id`
+	FROM `autoriai`
+	WHERE `pavarde` = 'tolkien' OR `pavarde` = 'martin'
+)
+
+-- LIKE kai ieškom tik pagal dalį reikšmės
+SELECT * FROM `autoriai` WHERE `vardas` LIKE 'J%'
+SELECT * FROM `autoriai` WHERE `vardas` LIKE '%as'
+SELECT * FROM `autoriai` WHERE `pavarde` LIKE '%a%'
+SELECT * FROM `autoriai` WHERE `pavarde` LIKE '%jon%' OR `vardas` LIKE '%jon%'
+SELECT * FROM `autoriai` WHERE `pavarde` LIKE 'Mar___'
+
+-- BETWEEN leidžia pasirinkti reikšmes tam tikrame rėžyje. Sąlygos reikšmės įeina į rėžį
+SELECT * FROM `knygos` WHERE `kaina` BETWEEN 10 AND 20
+SELECT * FROM `knygos` WHERE `puslapiu_skaicius` BETWEEN 200 AND 400
+SELECT * FROM `knygos` WHERE `puslapiu_skaicius` > 200 AND `puslapiu_skaicius` < 400
+SELECT * FROM `knygos` WHERE `puslapiu_skaicius` BETWEEN 279 AND 678
+SELECT * FROM `knygos` WHERE `puslapiu_skaicius` >= 279 AND `puslapiu_skaicius` <= 678
+
+-- Komandos turi neiginį NOT
+SELECT * FROM `knygos` WHERE `kaina` NOT IN (7.80, 12.50, 50.99)
+SELECT * FROM `autoriai` WHERE `vardas` NOT LIKE 'J%'
+SELECT * FROM `knygos` WHERE `kaina` NOT BETWEEN 10 AND 20
+
+-- Rikiavimas pagal tam tikrą stulpelį/-ius. ASC - abėcėlės/didėjančia tvarka, DESC - atvirkščiaiai
+SELECT * FROM `autoriai` ORDER BY `vardas`, `pavarde` DESC
+SELECT * FROM `knygos` ORDER BY `kaina` DESC
+SELECT * FROM `autoriai` ORDER BY `vardas` ASC, `pavarde` DESC;
+
+-- DISTINCT skirtas isvesti tik nepasikartojancias reiksmes (unikalias)
+SELECT DISTINCT `vardas` FROM `autoriai` ORDER BY `vardas` ASC
+
+-- COUNT skaiciujam kiek eiluciu duomenu turime lenteleje.
+SELECT COUNT(*) FROM `knygos` WHERE `puslapiu_skaicius` > 300
+
+
+-- Agregacijų funkcijos
+SELECT ROUND(AVG(`puslapiu_skaicius`)) AS 'vidutinis_knygos_puslapiu_skaicius' FROM `knygos`
+SELECT SUM(`puslapiu_skaicius`) AS 'vidutinis_knygos_puslapiu_skaicius' FROM `knygos`
+SELECT MAX(`puslapiu_skaicius`) AS 'vidutinis_knygos_puslapiu_skaicius' FROM `knygos`
+SELECT MIN(`puslapiu_skaicius`) AS 'vidutinis_knygos_puslapiu_skaicius' FROM `knygos`
+
+-- Grupuojame pagal vardus ir skaičiujame kiek yra autorių su atititinkamais vardais
+SELECT `vardas`, COUNT(`id`) FROM `autoriai` GROUP BY `vardas`
+
+-- HAVING Leidzia atfiltruoti duomenis jau po grupavimo
+SELECT `vardas`, COUNT(`id`) AS 'kiekis'
+FROM `autoriai`
+WHERE `vardas` != 'J. R. R.'
+GROUP BY `vardas`
+HAVING `kiekis` = 1
+ORDER BY `vardas`
+
+
+-- Apjungiam lenteles su JOIN komanda
+SELECT *
+FROM `knygos`
+JOIN `knygu_autoriai` ON `knygos`.`id` = `knygu_autoriai`.`knygos_id`
+JOIN `autoriai` ON `knygu_autoriai`.`autoriaus_id` = `autoriai`.`id`
+
+-- Apjungtom lentelem galim naudoti visas pries tai suzinotas komandas
+SELECT
+	COUNT(`knygos`.`id`) AS 'knygu_kiekis',
+	SUM(`knygos`.`puslapiu_skaicius`) AS 'puslapiu_suma',
+	`autoriai`.`vardas`,
+	`autoriai`.`pavarde`
+FROM `knygos`
+JOIN `knygu_autoriai` ON `knygos`.`id` = `knygu_autoriai`.`knygos_id`
+JOIN `autoriai` ON `knygu_autoriai`.`autoriaus_id` = `autoriai`.`id`
+GROUP BY `autoriai`.`id`
+
+-- Naujo iraso ikelimas į lentą
+INSERT INTO `knygos`
+(`pavadinimas`, `kaina`, `puslapiu_skaicius`)
+VALUES
+('Brisiaus Galas', 12.45, 7),
+('Kliudžiau', 11.68, 6);
+-- Naujo iraso ikelimas į lentą
+INSERT INTO `knygu_autoriai`
+(`knygos_id`, `autoriaus_id`)
+VALUES
+(8, 7),
+(9, 7);
+
+-- Įrašo duomenų atnaujinimas
+UPDATE `knygos`
+SET `pavadinimas` = 'SOSTŲ KARAI: Sostų žaidimas. Ledo ir ugnies giesmė.'
+WHERE `id` = 4;
---
+++ paskaitos/php_09/knygos.php
@@ -0,0 +1,96 @@
+<?php include 'header.php'; ?>
+<?php include 'navbar.php'; ?>
+
+        <header class="bg-light text-dark pt-5 pb-5">
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                        <h1 class="display-1">Knygos</h1>
+                        <p class="lead">Visos suvestos knygos</p>
+                    </div>
+                </div>
+            </div>
+        </header>
+
+        <div class="content pt-5 pb-5">
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                        <p><a href="nauja.html" class="btn btn-success">Nauja knyga</a></p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <table class="table table-hover">
+                            <thead>
+                                <tr>
+                                    <th scope="col">#</th>
+                                    <th scope="col">Pavadinimas</th>
+                                    <th scope="col">Autorius</th>
+                                    <!-- <th scope="col">Žanras</th> -->
+                                    <th scope="col">Puslapių sk.</th>
+                                    <th scope="col">Kaina, €</th>
+                                    <th scope="col">Veiksmai</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <th scope="row">1</th>
+                                    <td>Stulpelis</td>
+                                    <td>Stulpelis</td>
+                                    <!-- <td>Stulpelis</td> -->
+                                    <td>Stulpelis</td>
+                                    <td>Stulpelis</td>
+                                    <td>
+                                        <a href="perziureti.html" class="btn btn-primary">Žiūrėti</a>
+                                        <a href="atnaujinti.html" class="btn btn-warning">Atnaujinti</a>
+                                        <a href="salinti.html" class="btn btn-danger">Šalinti</a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">2</th>
+                                    <td>Stulpelis</td>
+                                    <td>Stulpelis</td>
+                                    <!-- <td>Stulpelis</td> -->
+                                    <td>Stulpelis</td>
+                                    <td>Stulpelis</td>
+                                    <td>
+                                        <a href="perziureti.html" class="btn btn-primary">Žiūrėti</a>
+                                        <a href="atnaujinti.html" class="btn btn-warning">Atnaujinti</a>
+                                        <a href="salinti.html" class="btn btn-danger">Šalinti</a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">3</th>
+                                    <td>Stulpelis</td>
+                                    <td>Stulpelis</td>
+                                    <!-- <td>Stulpelis</td> -->
+                                    <td>Stulpelis</td>
+                                    <td>Stulpelis</td>
+                                    <td>
+                                        <a href="perziureti.html" class="btn btn-primary">Žiūrėti</a>
+                                        <a href="atnaujinti.html" class="btn btn-warning">Atnaujinti</a>
+                                        <a href="salinti.html" class="btn btn-danger">Šalinti</a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">4</th>
+                                    <td>Stulpelis</td>
+                                    <td>Stulpelis</td>
+                                    <!-- <td>Stulpelis</td> -->
+                                    <td>Stulpelis</td>
+                                    <td>Stulpelis</td>
+                                    <td>
+                                        <a href="perziureti.html" class="btn btn-primary">Žiūrėti</a>
+                                        <a href="atnaujinti.html" class="btn btn-warning">Atnaujinti</a>
+                                        <a href="salinti.html" class="btn btn-danger">Šalinti</a>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+<?php include 'footer.php'; ?>
